81b9b35eb775ede4cd348de4a52e3edd3f409291
jgarnicaa <jgarnicaa@unal.edu.co>
2025-01-21 17:07:36 +0100

    web scraping ES

---
+++ src/web scraping/ES_Poetry_PDA.py
@@ -0,0 +1,46 @@
+# ------ File did to scrape www.poemas-del-alma.com -----------
+
+### In this page we have authors and each one has different poems. So the first step is get all the autors list.
+
+import requests
+import bs4
+
+## make a list of tuples between all authors and their links to access
+def get_authors(char_init): ## char_init = initial char of author's name
+
+    url = 'https://www.poemas-del-alma.com/{char}.html'.format(char=char_init)
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.text, features="html.parser")
+
+    results=[]
+    content=soup.find_all("a", href=True) #Find all names with associated links
+
+    for label in content:
+       if url[:-5] in label['href']: # Take only URL that match with inital char of author
+           results.append((label['href'], label.text))
+
+    return (results)
+
+def get_link_poems(author_url):
+
+    r = requests.get(author_url)
+    soup = bs4.BeautifulSoup(r.text, features="html.parser")
+
+    results=[]
+    content=soup.find_all("a", href=True) #Find all names with associated links
+    for label in content:
+        if "blog" not in label['href']: # Take only URL that match with inital char of author
+           results.append((label['href'], label.text))
+
+    return results
+    
+
+
+def main():
+    authors=get_authors('a')
+    link, name = authors[0]
+    html=get_link_poems(link)
+    print(html)
+
+if __name__== "__main__":
+    main()
