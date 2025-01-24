e65fd00b619a2533ddb8620f685cc7873b15024a
jgarnicaa <jgarnicaa@unal.edu.co>
2025-01-24 15:41:05 +0100

    ES web scraping updated

--- src/web scraping/ES_Poetry_PDA.py
+++ src/web scraping/ES_Poetry_PDA.py
@@ -4,6 +4,8 @@
 
 import requests
 import bs4
+import pandas as pd
+import csv
 
 ## make a list of tuples between all authors and their links to access
 def get_authors(char_init): ## char_init = initial char of author's name
@@ -19,7 +21,7 @@ def get_authors(char_init): ## char_init = initial char of author's name
        if url[:-5] in label['href']: # Take only URL that match with inital char of author
            results.append((label['href'], label.text))
 
-    return (results)
+    return results
 
 def get_link_poems(author_url):
 
@@ -38,25 +40,63 @@ def get_link_poems(author_url):
 def get_poems(poems_url): ## A list full of poems links, it's necessary complete links
 
     results=[]
+    poems_content=[]
+    poems_titles=[]
     for poem_url in poems_url:
         
         poem_url='https://www.poemas-del-alma.com/'+poem_url
+
+        print(poem_url)
         r = requests.get(poem_url)
         soup=bs4.BeautifulSoup(r.text, features="html.parser")
-        poem_title=soup.find("h1").get_text()
+        poem_title=soup.find("h1", class_="title-poem").get_text()
         poem_content=soup.find("p").get_text(separator='\n')
-        results.append((poem_title, poem_content))
+        poems_content.append(poem_content)
+        poems_titles.append(poem_title)
+        
+    results.append(poems_titles)
+    results.append(poems_content)
 
     return results
 
 
+def get_dataset(name, titles, poem):
+
+    
+    df = pd.DataFrame({
+        "Title": titles,
+        "Poem": poem,
+        "Author": name
+
+    })
+
+    return df
+
+
+
 def main():
-    authors=get_authors('a')
-    link_author, name_author = authors[6]
-    print("Author's name: ", name_author)
-    poems_url = get_link_poems(link_author)
-    poems = get_poems(poems_url)
-    print(poems)
+    abc=['a', 'b', 'c', 'd', 'e', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    authors = [author for lettre in abc for author in get_authors(lettre)]
+
+    with open("PoemasDelAlmaDataset.csv", 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames= ["Title", "Poem", "Author"]
+        writer=csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+
+        for author in authors:
+            link_author, name_author = author
+            poems_url = get_link_poems(link_author)
+            titles_poem, content_poems = get_poems(poems_url)
+            for i in range(len(titles_poem)):
+                writer.writerow({'Title':titles_poem[i], 
+                                 'Poem':content_poems[i], 
+                                 "Author":name_author})
+        #dataset_temp=get_dataset(name_author,titles_poem,content_poems)
+    print("Done")
+        #dataset = pd.concat([dataset, dataset_temp], ignore_index=True)
+
+    #print(dataset)
+
 
 if __name__== "__main__":
     main()
